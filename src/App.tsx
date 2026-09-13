801c0316daf1c3939b8eba5d17c7625d14abc46c
Shahzaib Hassan <shahzaibdiraj480@gmail.com>
2026-09-13 14:53:52 +0500

    Tenzies: SetUP

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { useRef, type FormEvent } from "react";
 import "./index.css";
 
+import ReactDOM from "react-dom"
 import logo from "./logo.svg";
 import reactLogo from "./react.svg";
+import {  MainApp } from "./Components/MainApp";
 
 
 export function App() {
   return (
     <div>
-
+      <MainApp/>
     </div>
   );
 }
---
+++ src/Components/MainApp.tsx
@@ -0,0 +1,10 @@
+export function MainApp(){
+    return(
+        <>
+        <main>
+            Type  SomeThing 
+            
+            </main>
+        </>
+    );
+}
