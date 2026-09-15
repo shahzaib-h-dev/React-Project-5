e988abd0052844364384e8b80acc1e16617de60f
Shahzaib Hassan <shahzaibdiraj480@gmail.com>
2026-09-15 19:35:26 +0500

    Change dice to Objects

--- src/Components/MainApp.tsx
+++ src/Components/MainApp.tsx
@@ -8,14 +8,14 @@ export function MainApp(){
 
    function generateAllNewDice(){
     return new Array(10).fill(0)
-    .map(() => Math.ceil(Math.random() * 6))
+    .map(() => ({value: Math.ceil(Math.random() * 6), isHeld: false}))
     }
    
    function rollDice(){
     setDice(generateAllNewDice)
    }
 
- const diceElements = dice.map( num => <Die value={num}/>) 
+ const diceElements = dice.map( dieObj => <Die value={dieObj.value}/>) 
     return (
         <main>
             <div className="die-container">
