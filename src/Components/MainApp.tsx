3646b4cf97f383e0021270681257120a062b1217
Shahzaib Hassan <shahzaibdiraj480@gmail.com>
2026-09-15 14:48:11 +0500

    Tenzies: Generate 10 random numbers

--- src/Components/MainApp.tsx
+++ src/Components/MainApp.tsx
@@ -1,6 +1,13 @@
 import { Die } from "./Die";
 
 export function MainApp(){
+
+   function generateAllNewDice(){
+    return new Array(10).fill(0)
+    .map(() => Math.ceil(Math.random()+ 6))
+    }
+   
+
     return (
         <main>
             <div className="die-container">
